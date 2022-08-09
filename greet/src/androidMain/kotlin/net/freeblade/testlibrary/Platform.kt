a5386a5ab7f40e9b1e20497425429ec58866bc2a
ChangJun Quan <changjun.quan@bsci.com>
2022-08-09 14:23:22 +0800

    Initial commit

---
+++ greet/src/androidMain/kotlin/net/freeblade/testlibrary/Platform.kt
@@ -0,0 +1,5 @@
+package net.freeblade.testlibrary
+
+actual class Platform actual constructor() {
+    actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
---
+++ greet/src/commonMain/kotlin/net/freeblade/testlibrary/Platform.kt
@@ -0,0 +1,5 @@
+package net.freeblade.testlibrary
+
+expect class Platform() {
+    val platform: String
+}
